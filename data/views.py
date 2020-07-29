336db13eed14bf2f0e8e43dbdd6b2ec6ed740a1e
rnikolai <nikolai.rubtcov@repustate.com>
2020-07-29 16:02:40 -0400

    Project Routing

--- data/views.py
+++ data/views.py
@@ -1,35 +1,46 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
+from data.models import Data, Project
 from django import template
+
 
 @login_required(login_url="/login/")
 def index(request):
-    return render(request, "index.html")
+    a = Project.objects.filter(
+        users=request.user).values_list('id').order_by('id')[0][0]
 
-@login_required(login_url="/login/")
+    return redirect('/projects/'+str(a)+'/')
+
+
+@ login_required(login_url="/login/")
 def pages(request):
+
     context = {}
+
     # All resource paths end in .html.
     # Pick out the html file name from the url. And load that template.
     try:
-        
+
         load_template = request.path.split('/')[-1]
-        html_template = loader.get_template( load_template )
+        html_template = loader.get_template(load_template)
         return HttpResponse(html_template.render(context, request))
-        
+
     except template.TemplateDoesNotExist:
 
-        html_template = loader.get_template( 'page-404.html' )
+        html_template = loader.get_template('page-404.html')
         return HttpResponse(html_template.render(context, request))
 
     except:
-    
-        html_template = loader.get_template( 'page-500.html' )
+
+        html_template = loader.get_template('page-500.html')
         return HttpResponse(html_template.render(context, request))
+
+
+def projects(request, num=-1):
+    a = Project.objects.filter(users=request.user)
+    print(a)
+    context = {'project_list': list(a.values())}
+
+    return render(request,  "project.html", context)
