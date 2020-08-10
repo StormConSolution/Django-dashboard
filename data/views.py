542ef494528ca66209cd3723de6783865f29e807
rnikolai <nikolai.rubtcov@repustate.com>
2020-08-10 10:54:54 -0400

    chart data is made in a different function

--- data/views.py
+++ data/views.py
@@ -1,12 +1,10 @@
-from data.models import Data, Project
-
 from django import template
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
-from data.models import Data, Project
+from data.models import Data, Project, Aspect, Entity
 from django import template
 from django.core.serializers.json import DjangoJSONEncoder
 
@@ -57,41 +55,44 @@ def pages(request):
         return HttpResponse(html_template.render(context, request))
 
 
-@login_required(login_url=LOGIN_URL)
-def projects(request, project_id):
-    list_of_projects = list(
-        Project.objects.filter(users=request.user).values())
-    this_project = get_object_or_404(Project, pk=project_id)
-
-    if this_project.users.filter(pk=request.user.id).count() == 0:
-        # This user does not have permission to view this project.
-        return HttpResponseForbidden()
+def get_chart_data(this_project):
 
     # TODO: aspect query
     # Aspect.objects.filter(data__project=this_project,
     # data__date_created__range=(start,
     # end)).order_by('label').annotate(total_count=Sum('count'))
 
-    data_points = list(Data.objects.filter(project=project_id).values())
-
     # Django standart date formater
     """ 
     print(json.dumps(
-        data_points[0]['date_created'],
+        datetime,
         sort_keys=True,
         indent=1,
         cls=DjangoJSONEncoder
     ))
     """
-    # Change date to ISO 8601 for js
-    for i in data_points:
-        i['date_created'] = json.dumps(i['date_created'], default=date_handler)
+    return {}
 
+
+@login_required(login_url=LOGIN_URL)
+def projects(request, project_id):
+
+    this_project = get_object_or_404(Project, pk=project_id)
+    if this_project.users.filter(pk=request.user.id).count() == 0:
+        # This user does not have permission to view this project.
+        return HttpResponseForbidden()
+
+    if request.method == 'POST':
+
+        return JsonResponse(get_chart_data(this_project))
+
+    # Project name for the tab name
     context = {'project_name': this_project}
-    data = json.dumps({"data": data_points})
-    if data_points:
-        context['project_data'] = data
-    if list_of_projects:
-        context['project_list'] = list_of_projects
+
+    # List of projects for the sidebar
+    context['project_list'] = list(
+        Project.objects.filter(users=request.user).values())
+
+    context['chart_data'] = get_chart_data(this_project)
 
     return render(request,  "project.html", context)
