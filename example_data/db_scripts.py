72f24132a1b374c6943751d8020d51ec1b099b2a
rnikolai <nikolai.rubtcov@repustate.com>
2020-08-11 14:12:43 -0400

    Added Fixture

---
+++ example_data/db_scripts.py
@@ -0,0 +1,79 @@
+
+import django
+import os
+import json
+import requests
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
+django.setup()
+
+# Model imports have to be after 
+from data.models import *
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
+APIKEY = 'repustatedemopage'
+
+def add_data(project, source, text, lang, with_entities=False, aspect_model=None,aspect=None):
+
+    sentiment = requests.post('http://api.repustate.com/v4/{APIKEY}/score.json'.format(APIKEY = APIKEY), {'text': text, 'lang': lang}).json()['score']
+
+    entities = None
+    if with_entities:
+        entities = requests.post('http://api.repustate.com/v4/{APIKEY}/entities.json'.format(APIKEY = APIKEY), {'text': text, 'lang': lang}).json()
+    
+    aspects = None
+    if aspect_model is not None:
+        aspects = requests.post('http://api.repustate.com/v4/{APIKEY}/aspect.json'.format(APIKEY = APIKEY), {'text': text, 'lang': lang, 'model': aspect_model}).json()
+    else:
+        aspects = json.loads(aspect)
+
+    data = Data.objects.create(
+        project=project,
+        source=source,
+        text=text,
+        sentiment=sentiment,
+        language=lang,
+        keywords = ()
+    )
+
+    for ent in entities['entities']:
+        entity_instance, created = Entity.objects.get_or_create(
+            data=data,
+            label=ent['title']
+        )
+        
+        for clas in ent['classifications']:
+            c_instance, created  = Classification.objects.get_or_create(
+                label = clas
+            )
+            entity_instance.classifications.add(c_instance)
+
+        data.entities.add(entity_instance)
+        
+    
+    for key, value in aspects.items():
+        if key != "status":
+            for v in value:
+                Aspect.objects.create(
+                    data=data,
+                    label=key,
+                    chunk=v['chunk'],
+                    sentiment=v['score'],
+                    topic=v['sentiment_topic'],
+                    sentiment_text=v['sentiment_text']
+                )
+
+def create_new_project(project_name):
+    this_project = Project.objects.create(name = project_name)
+    this_project.save()
+
+def assign_user_to_project(project_id,user_id):
+    this_project = get_object_or_404(Project, pk=project_id)
+    this_project.users.add(User.objects.get(id =user_id))
+    this_project.save
+
+def add_chart_to_project():
+    pass
+
+def remove_chart_from_project():
+    pass
---
+++ example_data/generate_fixture.py
@@ -0,0 +1,25 @@
+
+import django
+import os
+import db_scripts
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
+django.setup()
+
+# Model imports have to be after 
+from data.models import *
+
+
+Project.objects.all().delete()
+Data.objects.all().delete()
+
+db_scripts.create_new_project("Project 1")
+db_scripts.assign_user_to_project(Project.objects.get(name = 'Project 1').id,1)
+
+aspects = open('example_data/AR.txt').readlines()
+text = open('example_data/raw.txt')
+
+this_project = Project.objects.get(name = 'Project 1')
+
+for i,line in enumerate(text):
+    db_scripts.add_data(this_project,'twitter',line,'en',True,aspect_model=None,aspect=aspects[i])
