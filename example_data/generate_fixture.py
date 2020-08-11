76e6f21eec674c9665f3877f7b23e8f855e5d5c0
rnikolai <nikolai.rubtcov@repustate.com>
2020-08-11 15:14:25 -0400

    Added create user function + fromating

--- example_data/generate_fixture.py
+++ example_data/generate_fixture.py
@@ -14,7 +14,8 @@ Project.objects.all().delete()
 Data.objects.all().delete()
 
 db_scripts.create_new_project("Project 1")
-db_scripts.assign_user_to_project(Project.objects.get(name = 'Project 1').id,1)
+user_id = db_scripts.create_user()
+db_scripts.assign_user_to_project(Project.objects.get(name = 'Project 1').id,user_id)
 
 aspects = open('example_data/AR.txt').readlines()
 text = open('example_data/raw.txt')
