753e6ad6802980ca1d5d9f03833a3147e2b92e7b
philipdokic <hey@phildjokic.com>
2020-08-12 14:25:46 -0400

    added admin interface

--- data/admin.py
+++ data/admin.py
@@ -2,7 +2,12 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
 from django.contrib import admin
+from data.models import *
 
-# Register your models here.
+class DataAdmin(admin.ModelAdmin):
+    list_display = ('date_created', 'text', 'source', 'language', 'sentiment')
+    list_filter = ('source',)
+    search_fields = ('text',)
+
+admin.site.register(Data, DataAdmin)
