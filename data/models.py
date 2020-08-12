5a700a883f8c85b6552c118436a58a12677036ee
rnikolai <nikolai.rubtcov@repustate.com>
2020-08-12 11:19:20 -0400

    Data model,date no time

--- data/models.py
+++ data/models.py
@@ -55,7 +55,7 @@ class Entity(models.Model):
         return self.label
 
 class Data(models.Model):
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateField(auto_now_add=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
     source = models.CharField(max_length=80, blank=True)
