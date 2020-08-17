9cea45b13747c8d80c768190988c9c8296f53428
rnikolai <nikolai.rubtcov@repustate.com>
2020-08-17 14:16:27 -0400

    added new graps to script

--- example_data/add_all_charts.py
+++ example_data/add_all_charts.py
@@ -14,5 +14,7 @@ db_scripts.add_chart_to_project(id,"sentiment_t",2)
 db_scripts.add_chart_to_project(id,"aspect_f",1)
 db_scripts.add_chart_to_project(id,"aspect_t",2)
 db_scripts.add_chart_to_project(id,"aspect_s",2)
+db_scripts.add_chart_to_project(id,"sentiment_source",2)
+db_scripts.add_chart_to_project(id,'emotional_entities',2)
 
 
