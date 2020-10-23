63ce571b8c6c2491db41487d0a40595e2ccf234d
philipdokic <hey@phildjokic.com>
2020-10-23 14:46:45 -0400

    made sentiment enditable from change list

--- data/admin.py
+++ data/admin.py
@@ -53,6 +53,7 @@ class ProjectAdmin(admin.ModelAdmin):
 class DataAdmin(admin.ModelAdmin):
     list_display = ('date_created', 'text', 'source', 'language', 'sentiment')
     list_filter = ('project', 'source',)
+    list_editable = ('sentiment',)
     search_fields = ('text', 'emotionalentity__entity__label', 'emotionalentity__emotion__label',)
     readonly_fields = ('entities', 'language', 'text', 'source',)
     date_hierarchy = 'date_created'
