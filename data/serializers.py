32f2a281a054dacc0b73d1d3eb34b1d75e9fbd27
philipdokic <hey@phildjokic.com>
2021-01-06 10:43:22 -0500

    added serializer fields to related fields show up

--- data/serializers.py
+++ data/serializers.py
@@ -3,7 +3,13 @@ from rest_framework import serializers
 from data.models import Data
 
 class DataSerializer(serializers.HyperlinkedModelSerializer):
+
+    country = serializers.StringRelatedField(many=False)
+    source = serializers.StringRelatedField(many=False)
+    keywords = serializers.StringRelatedField(many=True)
+    entities = serializers.StringRelatedField(many=True)
     
     class Meta:
         model = Data
-        fields = ['date_created', 'text', 'sentiment', 'weighted_score', 'language',]
+        fields = ['date_created', 'country', 'source', 'text', 'keywords',
+                'sentiment', 'weighted_score', 'language', 'entities']
