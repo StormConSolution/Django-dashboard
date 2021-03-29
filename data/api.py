89482245b47a97ce32ab4de8e17b9208bfc94b36
philipdokic <hey@phildjokic.com>
2021-03-29 17:26:16 -0400

    aspect and source count

--- data/api.py
+++ data/api.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('country/', views.CountryListAPI.as_view()),
     path('project/', views.ProjectListView.as_view()),
     path('data/project/<int:project_id>/', views.ProjectDataListView.as_view(), name='project-data-list'),
-    path('sentiment-count/<int:project_id>/', views.sentiment_count),
+    path('project-overview/<int:project_id>/', views.project_overview),
     path('volume-by-source/<int:project_id>/', views.volume_by_source),
     path('co-occurence/<int:project_id>/', views.co_occurence),
     path('sentiment-per-aspect/<int:project_id>/', views.sentiment_per_aspect),
