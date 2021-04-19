91b4cca426bf913b034b4ff1f03aa73a42f484a0
philipdokic <hey@phildjokic.com>
2021-04-19 14:43:12 -0400

    add word clooud

--- data/api_views_folder/__init__.py
+++ data/api_views_folder/__init__.py
@@ -1,4 +1,5 @@
 from .entity_aspect_for_emotion import entity_aspect_for_emotion
 from .topics_per_aspect import topics_per_aspect
+from .keywords_count import keywords_count
 
-__all__ = ['entity_aspect_for_emotion', 'topics_per_aspect']
+__all__ = ['entity_aspect_for_emotion', 'topics_per_aspect', 'keywordscount']
---
+++ data/api_views_folder/keywords_count.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+from django.db import connection
+from django.http import JsonResponse
+import data.models as models
+
+LOGIN_URL = '/login/'
+
+@login_required(login_url=LOGIN_URL)
+def keywords_count(request, project_id):
+    this_project = get_object_or_404(models.Project, pk=project_id)
+    if this_project.users.filter(pk=request.user.id).count() == 0:
+        raise PermissionDenied
+
+    response = []
+    with connection.cursor() as cursor:
+        cursor.execute("""select * from get_keyword_counts(%s) limit 20""", [project_id])
+        rows = cursor.fetchall()
+    for row in rows:
+        response.append({
+            'keyword':row[0],
+            'keywordCount':row[1],
+        })
+        
+    return JsonResponse(response, safe=False)
