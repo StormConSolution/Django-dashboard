5c2b577a80c7e8f2b9c99a062cca7464a7638780
philipdokic <hey@phildjokic.com>
2021-04-19 18:20:49 -0400

    updated keyword count function to accept arrays of languages or sources

--- sql/keyword_count.sql
+++ sql/keyword_count.sql
@@ -1,21 +1,16 @@
-/*
+drop function get_keyword_counts;
 
-SELECT * FROM get_keyword_counts(3155);
+-- select * from get_keyword_counts(3155, ARRAY['en', 'zh']);
 
-SELECT * FROM get_keyword_counts(3155, language := 'en', source_id := 187, classification_id := 1, aspect := 'Appearance', sql_filter := $SQL$date_created > '2019-01-01'$SQL$, max_rows := 10);
-
- */
-
-DROP FUNCTION IF EXISTS get_keyword_counts;
-CREATE OR REPLACE FUNCTION get_keyword_counts(project_id INT, language VARCHAR = NULL, source_id INT = NULL, classification_id INT = NULL, aspect TEXT = NULL, sql_filter TEXT = NULL, max_rows INT = NULL) RETURNS TABLE(keyword TEXT, keyword_count INT)
-LANGUAGE plpgsql
-PARALLEL SAFE
-AS $$
+create or replace function get_keyword_counts(project_id integer, languages character varying[] DEFAULT NULL, source_ids integer[] DEFAULT NULL, classification_id integer DEFAULT NULL::integer, aspect text DEFAULT NULL::text, sql_filter text DEFAULT NULL::text, max_rows integer DEFAULT NULL::integer) returns TABLE(keyword text, keyword_count integer)
+	parallel safe
+	language plpgsql
+as $$
 DECLARE
     filters TEXT[] = ARRAY['project_id = ' || project_id];
 BEGIN
-    IF (language IS NOT NULL) THEN filters = ARRAY_APPEND(filters, 'language = ' || quote_literal(language)); END IF;
-    IF (source_id IS NOT NULL) THEN filters = ARRAY_APPEND(filters, 'source_id = ' || quote_literal(source_id)); END IF;
+    IF (languages IS NOT NULL) THEN filters = ARRAY_APPEND(filters, 'language = ANY($1)'); END IF;
+    IF (source_ids IS NOT NULL) THEN filters = ARRAY_APPEND(filters, 'source_id = ANY($2)'); END IF;
     IF (sql_filter IS NOT NULL) THEN filters = ARRAY_APPEND(filters, sql_filter); END IF;
     IF (classification_id IS NOT NULL) THEN filters = ARRAY_APPEND(filters, 'id IN (SELECT dde.data_id FROM data_data_entities dde JOIN data_entity_classifications ec ON dde.entity_id = ec.entity_id WHERE ec.classification_id = ' || quote_literal(classification_id) || ')'); END IF;
     IF (aspect IS NOT NULL) THEN filters = ARRAY_APPEND(filters, 'id IN (SELECT data_id FROM data_aspect WHERE label = ' || quote_literal(aspect) || ')'); END IF;
@@ -32,6 +27,7 @@ BEGIN
         GROUP BY keyword
         ORDER BY keyword_count DESC
         ' || CASE WHEN max_rows IS NOT NULL THEN 'LIMIT ' || max_rows ELSE '' END
+        USING languages, source_ids
     ;
 END
 $$;
