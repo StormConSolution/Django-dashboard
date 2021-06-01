5bb5484823b8ae6715c9d57d2153f590718207e6
philipdokic <hey@phildjokic.com>
2021-06-01 18:47:02 -0400

    upload csv

--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -2,7 +2,7 @@ from .celery import app
 from django.conf import settings
 import requests
 from data import models
-import datetime
+from datetime import datetime
 @app.task
 def process_data(dict):
     try:
@@ -10,10 +10,9 @@ def process_data(dict):
             HOST=settings.API_HOST, APIKEY=settings.APIKEY), data={'text': dict["text"], 'lang': dict["lang"]}).json()
         if 'score' in resp:
             sentiment = resp['score']
-            data = models.Data(project_id=dict["project_id"], text=dict["text"], date_created=datetime.datetime.now(), sentiment=sentiment, source_id=1, weighted_score=0, relevance= 0)
-            print(data) 
+            source = models.Source.objects.get_or_create(label=dict["source"])[0]
+            project = models.Project.objects.get(pk=dict["project_id"])
+            data = models.Data(project=project, text=dict["text"], date_created=datetime.strptime(dict["date"], "%Y-%m-%d"), sentiment=sentiment, source=source, url=dict["url"], language=dict["lang"], weighted_score=0, relevance= 0, metadata=dict["metadata"])
             data.save()
-        
     except Exception as e:
         print(e)
-    print(resp)
--- data/data_views/csv.py
+++ data/data_views/csv.py
@@ -1,9 +1,8 @@
 from io import StringIO
 import csv
 import json
-import datetime
+from datetime import date, datetime
 
-from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -12,34 +11,49 @@ from django.conf import settings
 from django.core.files.base import ContentFile
 from dashboard.tasks import process_data
 
+from data import models
 
 
 @method_decorator(csrf_exempt, name="upload_csv")
 def csv_upload(request):
-    project_id = request.GET.get("project-id")
-    body_unicode = request.body.decode('utf-8')
-    body = json.loads(body_unicode)
-    date = datetime.datetime.now()
-    file_name = date.strftime("%Y-%m-%d_%H:%M:%S_") + project_id + ".csv"
-    csv_buffer = StringIO()
-    csv_writer = csv.writer(csv_buffer)
-    headers = []
-    if request.GET.get("client", "") != "browser":
-        body = body["data"]["validRows"]
-    for element in body[0].keys():
-        headers.append(element)
-    csv_writer.writerow(headers)
-    for element in body:
-        row = []
-        task_argument = {}
-        task_argument["lang"] = 'en'
-        for value in element:
-            if value == "lang":
-                task_argument["lang"] = element[value]
-            row.append(element[value])
-        task_argument["text"] = element["text"]
-        task_argument["project_id"] = project_id
-        process_data.delay(task_argument)
-        csv_writer.writerow(row)
+    try:
+        project_id = request.GET.get("project-id")
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        date = datetime.now()
+        file_name = date.strftime("%Y-%m-%d_%H:%M:%S_") + project_id + ".csv"
+        csv_buffer = StringIO()
+        csv_writer = csv.writer(csv_buffer)
+        headers = []
+        if request.GET.get("client", "") != "browser":
+            body = body["data"]["validRows"]
+        for element in body[0].keys():
+            headers.append(element)
+        csv_writer.writerow(headers)
+        for element in body:
+            row = []
+            task_argument = {}
+            task_argument["lang"] = 'en'
+            task_argument["url"] = ''
+            task_argument[""] = ''
+            task_argument["metadata"] = {}
+            for value in element:
+                if value == "lang":
+                    task_argument[value] = element[value]
+                elif value == "date":
+                    task_argument[value] = element[value]
+                elif value == "source":
+                    task_argument[value] = element[value]
+                elif value == "url":
+                    task_argument[value] = element[value]
+                else:
+                    task_argument["metadata"][value] = element[value]
+                row.append(element[value])
+            task_argument["text"] = element["text"]
+            task_argument["project_id"] = project_id
+            process_data.delay(task_argument)
+            csv_writer.writerow(row)
+    except Exception as e:
+        print(e)
     default_storage.save(file_name, ContentFile(csv_buffer.getvalue().encode('utf-8')))
     return HttpResponse("csv upload")
