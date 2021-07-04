0d68c6d205554a11750bd4fe5c5922f8971bf5ba
philipdokic <hey@phildjokic.com>
2021-07-04 13:40:26 -0400

    removed old chart code
    fixed another bug in empty dates

--- data/urls.py
+++ data/urls.py
@@ -14,18 +14,6 @@ urlpatterns = [
     path('add-data/<int:project_id>/', api_views.add_data, name='add-data'),
     path('alert/toggle/<int:aspect_rule_id>/', views.alert_rule_toggle, name='alert-rule-toggle'),
     
-    path('aspect-topics/<int:project_id>/',
-         views.aspect_topics, name='aspect-topics'),
-    path('aspect-topic-detail/<int:project_id>/',
-         views.aspect_topic_detail, name='aspect-topic-detail'),
-    path('aspect-topic-summary/<int:project_id>/',
-         views.aspect_topic_summary, name='aspect-topic-summary'),
-
-    path('entities/<int:project_id>/', views.entities, name='entities'),
-
-    path('data-entries/<int:project_id>/',
-         views.data_entries, name='data-entries'),
-    #path('data-per-aspect/<int:project_id>/', views.data_per_aspect, name='data-per-aspect'),
     path('topics-per-aspect/<int:project_id>/', views.topics_per_aspect, name='topics_per_project'),
     path('project/<int:project_id>/', views.new_project_details, name='project'),
     
