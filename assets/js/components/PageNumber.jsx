ae2aab808d9fd7b5f25c82cd9b63b2e7a4249586
philipdokic <hey@phildjokic.com>
2021-09-08 14:42:37 -0400

    fix pagination, fix tooltip by changing jquery import

--- assets/js/components/PageNumber.jsx
+++ assets/js/components/PageNumber.jsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom'
 //currentPageNumber, pageNumber, callBack, options
 const PageNumber = (props) => {
     let className = "";
-    if(props.currentPageNumber === props.pageNumber){
+    if (props.currentPageNumber === props.pageNumber) {
         className = "active"
     }
+    let onClickHandler = () => {
+        ReactDOM.unmountComponentAtNode(props.divElement)
+        props.callBack(props.pageNumber, props.options)
+    }
     return (
-        <li className={className} onClick={()=>{
-            ReactDOM.unmountComponentAtNode(props.divElement)
-            props.callBack(props.pageNumber, props.options)
-        }}><a style={{cursor:"pointer"}}>{props.pageNumber}</a></li>
+        <li className={className} onClick={isNaN(props.pageNumber) ? undefined : onClickHandler}><a
+            style={{cursor: isNaN(props.pageNumber) ? "auto" : "pointer"}}>{props.pageNumber}</a></li>
     );
 }
 
